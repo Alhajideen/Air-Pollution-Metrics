5fda66b1b84e0f5ed138f87bf36a4d73c31e7e11
Alhajideen <salifunurudeen834@gmail.com>
2022-11-21 11:10:32 -0600

    import Nav in homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>Air Pollution Index</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../props/Nav';
+
+function Home() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default Home;
