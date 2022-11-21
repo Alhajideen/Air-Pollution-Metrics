5c91b42f4ce838ec958d98ce4baa18df8513451a
Alhajideen <salifunurudeen834@gmail.com>
2022-11-21 10:57:30 -0600

    add slice.js and store.js

---
+++ src/redux/slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const polutionSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {},
+});
+
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default polutionSlice.reducer;
